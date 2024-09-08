5d911835b7aa35a62f4cb0a3c8ca6491129d7f56
MrRodOSmith <Rodney.Smith@mediasmithsllc.com>
2024-09-07 22:52:51 -0600

    Another last submission and it's on the same branch.

--- src/components/ThankYouCard.js
+++ src/components/ThankYouCard.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Thanks from './illustration-thank-you.svg'
 
 function ThankYouCard({ rating }) {
     return (
         <div className='thank-you-card'>
+            <img src={Thanks} alt="Thank You" />
             <h2>Thank You!</h2>
-            <p>You selected {rating} out of 5.</p>
-            <p>We appreciate your feedback.</p>
+            <p className='your-selection'>You selected {rating} out of 5.</p>
+            <p className='thanks-text'>
+                We appreciate your feedback you taking the time to give a rating.<br />
+                If you ever need more support, don't hesitate to get in touch.
+            </p>
         </div>
     );
 }
